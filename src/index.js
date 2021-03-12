abf5fef9173b00c8bf326a07691773668702a038
SteunHaus <pavel.evdokimenko@gmail.com>
2021-03-13 01:36:22 +0300

    fix: Update index.js with valid solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined || matrix.length == 0) return [];
+
+  let result = [];
+  let x = 0;
+  let resultIndex = 0;
+  for (let i = 0; i < matrix.length; i++) {
+
+    if (x == 0 || x == -1) {
+      while (x < matrix[i].length) {
+        result[resultIndex] = matrix[i][x];
+        resultIndex++;
+        x++;
+      }
+
+    } else if (x > 0){
+      x = matrix[i].length;
+      while (x >= 0) {
+        result[resultIndex] = matrix[i][x];
+        resultIndex++;
+        x--;
+      }
+    }
+  }
+
+  result = result.filter(function(element) {
+    return element !== undefined;
+  });
+  return result;
 }
